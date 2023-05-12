e2a9e9e4d76f59a9dd7b33d1cb8d2ecfb74ea232
derricksouthworth <derricksouthworth@gmail.com>
2023-05-11 22:25:12 -0700

    Update http client

--- ArtisanGemstoneIMS.WebUI/Client/Program.cs
+++ ArtisanGemstoneIMS.WebUI/Client/Program.cs
@@ -10,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped<ICustomersClient, CustomersClient>();
 builder.Services.AddScoped<IInventoriesClient, InventoriesClient>();
 builder.Services.AddScoped<IProductsClient, ProductsClient>();
 
